d4856229a0442c52cf2f56e5dbaa0270c63503e5
Xalgemn <miquelangelmartosvilella>
2024-11-29 13:55:13 +0100

    juntar mapa con navbar

--- EcoApp-Tailwind/src/components/Navbar.tsx
+++ EcoApp-Tailwind/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="bg-green-800 text-white">
+        <nav className="bg-green-800 text-white fixed top-0 left-0 w-full z-50">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
               {/* Logo */}
